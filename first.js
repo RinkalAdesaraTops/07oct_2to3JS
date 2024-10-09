38aa3b6fe653014b99a5e733d16021a972307289
vaibhavmulchandani21imca <vaibhavmulchandani.21.imca@iict.indusuni.ac.in>
2024-10-09 14:31:02 +0530

    07octnewbatch

---
+++ first.js
@@ -0,0 +1,3 @@
+document.write("Welcome to Tops...!!!")
+document.getElementById('demo').innerHTML = "<b>This is text -- innerHTML</b>"
+document.getElementById('txt').innerText = "<b>This is text -- innerText</b>"
---
+++ variable.js
@@ -0,0 +1,35 @@
+//Arithmetic operator -- +,-,*,/,%,**,++,--
+//nothing
+// a=10
+// b=20
+// add=a+b
+// document.write('Addition is '+add)
+// minus = b-a
+// document.write('<br> Minus is '+minus)
+// //% - modulus 
+// document.write('<br> Modulus is '+(7%3))
+// a=5
+// document.write('<br> Square is '+(a**2))
+// document.write('<br> Cube is '+(a**3))
+// var let const
+var a=25 //Redeclare & reassign
+document.write('<br> A is '+a)
+
+var a ="Hello"
+document.write('<br> A is '+a)
+
+const pi=3.14 //we cant redeclare & reassign
+document.write('<br> Pi is '+pi)
+
+// const pi = 200
+// document.write('<br> Pi is '+pi)
+
+let name = "Test" //cant redeclare but reassign possibles
+document.write('<br> Name is '+name)
+
+name="abc"
+document.write('<br> Name is '+name)
+
+
+
+
